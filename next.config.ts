98dec43cc33b9fc61c8b0648eb91fe32f595dacd
Tsianda Netsianda <ntstsi008@myuct.ac.za>
2025-12-14 04:37:10 +0200

    removed eslint block

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // Removed the eslint block
+ 
 };
 
 export default nextConfig;
